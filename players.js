e17582ef29031c7f87b7305a1c076b344e94aa50
waljoel <34751092+waljoel@users.noreply.github.com>
2017-12-21 17:48:13 -0300

    Create players.js

---
+++ players.js
@@ -0,0 +1,6 @@
+players =  players = [
+			{"Name":"Cristiano Ronaldo","Age":32,"Photo":"https:\/\/cdn.sofifa.org\/48\/18\/players\/20801.png","Overall":94,"Value":95000000,"Acceleration":89,"Finishing":94},
+			{"Name":"L. Messi","Age":30,"Photo":"https:\/\/cdn.sofifa.org\/48\/18\/players\/158023.png","Overall":93,"Value":105000000,"Acceleration":92,"Finishing":95},
+			{"Name":"Neymar","Age":25,"Photo":"https:\/\/cdn.sofifa.org\/48\/18\/players\/190871.png","Overall":92,"Value":123000000,"Acceleration":94,"Finishing":89},
+			{"Name":"L. Su\u00e1rez","Age":30,"Photo":"https:\/\/cdn.sofifa.org\/48\/18\/players\/176580.png","Overall":92,"Value":97000000,"Acceleration":88,"Finishing":94}
+		];
